14e6d3ac79970e197af130385a6e9302e0f11a00
Rudra9991 <rurasish.das0@gmail.com>
2024-05-31 00:24:49 +0530

    added profile

--- docs/server/chunk-2LMR3PVK.mjs
+++
@@ -1,6 +0,0 @@
-import './polyfills.server.mjs';
-import{a as p,b as e,c as r,d as a,e as o,f as m,k as s,n as c,p as d,q as l,t as f,z as g}from"./chunk-YSKXRZPQ.mjs";var u="h1[_ngcontent-%COMP%]{background-color:green}div[_ngcontent-%COMP%]{background-color:#add8e6}p[_ngcontent-%COMP%]{background-color:#ff0}",v=(()=>{let t=class t{constructor(){this.title="digital-profile-project"}};t.\u0275fac=function(i){return new(i||t)},t.\u0275cmp=p({type:t,selectors:[["app-root"]],standalone:!0,features:[m],decls:10,vars:0,template:function(i,M){i&1&&(e(0,"html"),a(1,"head"),e(2,"body")(3,"h1"),o(4,"Rudrasish Das"),r(),e(5,"div"),o(6,`
-This is a text inside a div element.
-`),e(7,"p"),o(8,"This paragraph has its own background color."),r(),o(9,`
-We are still in the div element.
-`),r()()())},styles:[u,u]});let n=t;return n})();var h=[];var C={providers:[s({eventCoalescing:!0}),g(h),l()]};var y={providers:[f()]},x=c(C,y);var S=()=>d(v,x),q=S;export{q as a};
