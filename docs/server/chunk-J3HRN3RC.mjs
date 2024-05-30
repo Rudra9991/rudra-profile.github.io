33015ba2a2c104ead01658436e313dd789b0c058
Rudra9991 <rurasish.das0@gmail.com>
2024-05-30 22:57:43 +0530

    commit

---
+++ docs/server/chunk-J3HRN3RC.mjs
@@ -0,0 +1,6 @@
+import './polyfills.server.mjs';
+import{a as p,b as t,c as r,d as a,e as o,f as m,k as c,n as s,p as l,q as d,t as f,z as g}from"./chunk-YSKXRZPQ.mjs";var u="h1[_ngcontent-%COMP%]{background-color:green}div[_ngcontent-%COMP%]{background-color:#add8e6}p[_ngcontent-%COMP%]{background-color:#ff0}",v=(()=>{let e=class e{constructor(){this.title="digital-profile-project"}};e.\u0275fac=function(i){return new(i||e)},e.\u0275cmp=p({type:e,selectors:[["app-root"]],standalone:!0,features:[m],decls:10,vars:0,template:function(i,k){i&1&&(t(0,"html"),a(1,"head"),t(2,"body")(3,"h1"),o(4,"CSS background-color example!"),r(),t(5,"div"),o(6,`
+This is a text inside a div element.
+`),t(7,"p"),o(8,"This paragraph has its own background color."),r(),o(9,`
+We are still in the div element.
+`),r()()())},styles:[u,u]});let n=e;return n})();var C=[];var h={providers:[c({eventCoalescing:!0}),g(C),d()]};var S={providers:[f()]},x=s(h,S);var y=()=>l(v,x),q=y;export{q as a};
