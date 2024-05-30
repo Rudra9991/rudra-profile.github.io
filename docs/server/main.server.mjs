7506a0b34ecb7cdda2b46cbd22d8352c15aa8378
Rudra9991 <rurasish.das0@gmail.com>
2024-05-30 22:39:49 +0530

    build the project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5CCGNMUV.mjs";import"./chunk-GYSYMNES.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-2MFNCZ4L.mjs";import"./chunk-33DM4LQQ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
