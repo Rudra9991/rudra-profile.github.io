85187c391a138d14f5db2ba56c5c9a17f2a0ee06
Rudra9991 <rurasish.das0@gmail.com>
2024-07-19 22:30:44 +0530

    fixed view issue

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SFBKUCIZ.mjs";import"./chunk-YSKXRZPQ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-FYPBPYNE.mjs";import"./chunk-YSKXRZPQ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
